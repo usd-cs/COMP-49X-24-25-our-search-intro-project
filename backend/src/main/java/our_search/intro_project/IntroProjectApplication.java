3993ac8cc4ff91bc0679612c09d0c6531ddb9586
aescudero <aescudero@sandiego.edu>
2024-10-31 15:03:17 -0700

    Added the boilerplate cade for the backend app

---
+++ backend/src/main/java/our_search/intro_project/IntroProjectApplication.java
@@ -0,0 +1,13 @@
+package our_search.intro_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/our_search/intro_project/IntroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package our_search.intro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
