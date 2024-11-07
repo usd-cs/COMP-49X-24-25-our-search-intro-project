a9a6923537b37b30fcae036116bedf3079d54b0d
natjungquist <njungquist@sandiego.edu>
2024-11-07 15:06:31 -0800

    removed boilerplate code from App.js and added dependency config for testing

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import axios from 'axios';
+
+jest.mock('axios');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
