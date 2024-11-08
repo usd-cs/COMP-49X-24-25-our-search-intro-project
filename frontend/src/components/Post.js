db1b5c03efbfcdd89d38923b85d9601e577518db
natjungquist <njungquist@sandiego.edu>
2024-11-07 20:01:23 -0800

    initialized components related to viewing posts

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Post = ({ post }) => {
+
+//   return (
+   // the contents of the post
+//   );
+};
+
+export default Post;
