19cbbaddb59aebde4f4b52c2194bfec3c86595da
natjungquist <njungquist@sandiego.edu>
2024-11-09 12:12:07 -0800

    commented out state vars in Login in attempt to allow compilation

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
 function Login({ handleLogin, setShowLogin }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    // const [email, setEmail] = useState(''); 
+    // const [password, setPassword] = useState('');
 
-    const handleSubmit = async () => {
+    const handleSubmit = (e) => {
         // calls the prop handleLogin with email and password
-      };
+    };
 
     return (
+        <>
         <form onSubmit={handleSubmit}>
 
         </form>
+        </>
 
         // inside form:
         // user input 1: placeholder text = email
         // user input 2: placeholder text = password
         // submit event: button that says 'login'
-        // button that says 'cancel' - onClick={() => setShowLogin(false)}
+        // button that says 'back' - onClick={handleBack}
+
+        // after the user hits submit to log in, if successful, setShowLogin(false) to stop showing the login form
+        // if not successful, display error message 
     );
 }
 
