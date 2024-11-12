768f82b4609d1a66d2514b5955a8b4722eb928a6
Sharthok <rpal@sandiego.edu>
2024-11-12 15:45:42 -0800

    Reverted accidental merge

--- backend/src/main/java/our_search/intro_project/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package our_search.intro_project.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import our_search.intro_project.entities.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
